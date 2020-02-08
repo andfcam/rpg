7badab998656840a9c5901e73828dc78deb28ce1
elkdev <afcameron@gmail.com>
2020-02-08 15:53:25 +0000

    WASD control, movement

--- app.js
+++ app.js
@@ -9,24 +9,55 @@ app.use('/client', express.static(__dirname + '/client'));
 const http = require('http').createServer(app);
 const io = require('socket.io')(http);
 
-const PLAYER_LIST = {}; // {[id: socket, id: socket, id: socket]}
+const SOCKET_LIST = {}; // {[id: socket, id: socket, id: socket]}
+const PLAYER_LIST = {};
 const fps = 50;
 
 http.listen(2000, function () {
     console.log("Server started");
 });
 
-io.on('connection', function (player) {
-    player.id = Math.floor(100 * Math.random()); // TODO: Make id equal to user id
-    player.x = 0;
-    player.y = 0;
+class Player {
+    constructor(id) {
+        this.id = id;
+        this.x = 250;
+        this.y = 250;
+        this.maxSpeed = 5;
+        this.pressingUp = false;
+        this.pressingDown = false;
+        this.pressingLeft = false;
+        this.pressingRight = false;
+    }
 
+    updatePosition() {
+        if (this.pressingUp) this.y -= this.maxSpeed;
+        if (this.pressingDown) this.y += this.maxSpeed;
+        if (this.pressingLeft) this.x -= this.maxSpeed;
+        if (this.pressingRight) this.x += this.maxSpeed;
+    }
+}
+
+io.on('connection', function (socket) {
+    socket.id = Math.floor(100 * Math.random()); // TODO: Make id equal to user id
+    SOCKET_LIST[socket.id] = socket;
+
+    const player = new Player(socket.id);
     PLAYER_LIST[player.id] = player;
 
     console.log('User connected');
 
-    player.on('disconnect', function () {
+    socket.on('keyPress', function (event) {
+        switch (event.key) {
+            case 'up': player.pressingUp = event.state; break;
+            case 'down': player.pressingDown = event.state; break;
+            case 'left': player.pressingLeft = event.state; break;
+            case 'right': player.pressingRight = event.state; break;
+        }
+    });
+
+    socket.on('disconnect', function () {
         delete PLAYER_LIST[player.id];
+        delete SOCKET_LIST[socket.id];
         console.log('User disconnected');
     });
 });
@@ -35,9 +66,8 @@ const interval = setInterval(function () {
     let players = [];
 
     for (const id in PLAYER_LIST) {
-        player = PLAYER_LIST[id];
-        player.x++;
-        player.y++;
+        const player = PLAYER_LIST[id];
+        player.updatePosition();
         players.push({
             id: player.id,
             x: player.x,
@@ -45,8 +75,8 @@ const interval = setInterval(function () {
         });
     }
 
-    for (const id in PLAYER_LIST) {
-        player = PLAYER_LIST[id];
-        player.emit('playerPositions', players);
+    for (const id in SOCKET_LIST) {
+        const socket = SOCKET_LIST[id];
+        socket.emit('playerPositions', players);
     }
 }, 1000 / fps);
