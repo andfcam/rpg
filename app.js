103839fc3a64507adf1b465bdd7d7326b7c5acfb
elkdev <afcameron@gmail.com>
2020-02-08 10:47:45 +0000

    Socket.io setup

--- app.js
+++ app.js
@@ -1,10 +1,25 @@
 const express = require('express');
 const app = express();
-const serv = require('http').Server(app);
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
 
 app.get('/', function (req, res) {
     res.sendFile(__dirname + '/client/index.html');
 });
 app.use('/client', express.static(__dirname + '/client'));
 
-serv.listen(2000);
+http.listen(2000, function () {
+    console.log("Server started");
+});
+
+io.on('connection', function (socket) {
+    console.log('User connected');
+
+    socket.on('disconnect', function () {
+        console.log('User disconnected');
+    });
+
+    socket.on('msg', function (data) {
+        console.log(data.message);
+    })
+});
