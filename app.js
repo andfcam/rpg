73afead935180dc53128fa23d99ad9fa49603766
elkdev <afcameron@gmail.com>
2020-02-08 15:16:32 +0000

    Multiple sockets

--- app.js
+++ app.js
@@ -1,25 +1,52 @@
 const express = require('express');
 const app = express();
-const http = require('http').createServer(app);
-const io = require('socket.io')(http);
 
 app.get('/', function (req, res) {
     res.sendFile(__dirname + '/client/index.html');
 });
 app.use('/client', express.static(__dirname + '/client'));
 
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
+
+const PLAYER_LIST = {}; // {[id: socket, id: socket, id: socket]}
+const fps = 50;
+
 http.listen(2000, function () {
     console.log("Server started");
 });
 
-io.on('connection', function (socket) {
+io.on('connection', function (player) {
+    player.id = Math.floor(100 * Math.random()); // TODO: Make id equal to user id
+    player.x = 0;
+    player.y = 0;
+
+    PLAYER_LIST[player.id] = player;
+
     console.log('User connected');
 
-    socket.on('disconnect', function () {
+    player.on('disconnect', function () {
+        delete PLAYER_LIST[player.id];
         console.log('User disconnected');
     });
-
-    socket.on('msg', function (data) {
-        console.log(data.message);
-    })
 });
+
+const interval = setInterval(function () {
+    let players = [];
+
+    for (const id in PLAYER_LIST) {
+        player = PLAYER_LIST[id];
+        player.x++;
+        player.y++;
+        players.push({
+            id: player.id,
+            x: player.x,
+            y: player.y
+        });
+    }
+
+    for (const id in PLAYER_LIST) {
+        player = PLAYER_LIST[id];
+        player.emit('playerPositions', players);
+    }
+}, 1000 / fps);
